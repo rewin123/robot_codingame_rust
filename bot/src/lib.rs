e5c74a12585480b3873e71bde24e93ba37f22883
a.yamaev@smartengines.com <a.yamaev@smartengines.com>
2022-12-22 09:13:13 +0300

    added pathfinder

--- bot/src/lib.rs
+++ bot/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+use std::fmt::Debug;
 use std::{path::Path, io::Read};
 use std::str::FromStr;
 
@@ -17,6 +19,7 @@ pub struct Tile {
     pub scrap_amount : i32,
     pub recycler : bool,
     pub units : i32,
+    pub delta_units : i32,
     pub owner : TileOwner
 }
 
@@ -26,7 +29,8 @@ impl Default for Tile {
             scrap_amount: 0, 
             recycler: false, 
             units: 0, 
-            owner: TileOwner::No
+            owner: TileOwner::No,
+            delta_units : 0
         }
     }
 }
@@ -56,12 +60,151 @@ pub enum Action {
     Build(BuildAction)
 }
 
+#[derive(Default, Clone, PartialEq, Eq, Debug)]
+pub struct TVec2<T : Default + Clone + PartialEq + Eq + Debug> {
+    pub x : T,
+    pub y : T
+}
+
+impl<T : Default + Clone + PartialEq + Eq + Debug> TVec2<T> {
+    pub fn new(x : T, y : T) -> TVec2<T> {
+        TVec2 {
+            x, 
+            y
+        }
+    }
+}
+
+impl TVec2<usize> {
+    fn dist(&self, other : &TVec2<usize>) -> f32 {
+        let dx = self.x as f32 - other.x as f32;
+        let dy = self.y as f32 - other.y as f32;
+        let dist = (dx * dx + dy * dy).sqrt();
+        dist
+    }
+}
+
+pub struct Pathfinder {
+    pub data : Vec<i32>,
+    pub walls : Vec<bool>,
+    pub prev_point : Vec<TVec2<usize>>,
+    pub w : usize,
+    pub h : usize
+}
+
+struct GrowPoint {
+    pub p : TVec2<usize>,
+    pub prev_p : TVec2<usize>,
+    pub val : i32
+}
+
+impl Pathfinder {
+
+    #[inline(always)]
+    pub fn get_idx(&self, x : usize, y : usize) -> usize {
+        y * self.w + x
+    }
+
+    pub fn find_path(&mut self, src : &TVec2<usize>, dst : &TVec2<usize>) -> TVec2<usize> {
+
+        self.data.fill(0);
+
+        let mut raise_points = VecDeque::new();
+        raise_points.push_back(GrowPoint { p : src.clone(), prev_p : src.clone(), val : 1 });
+
+        let d_w = self.w - 1;
+        let d_h = self.h - 1;
+
+        let mut nearest_point = src.clone();
+        let mut nearest_dist = 100000000.0_f32;
+
+        while raise_points.len() > 0 && self.data[dst.y * self.w + dst.x] == 0 {
+            let gp = raise_points.pop_front().unwrap();
+            let idx = gp.p.y * self.w + gp.p.x;
+            
+            if self.data[idx] == 0 && self.walls[idx] == false {
+                self.data[idx] = gp.val;
+                self.prev_point[idx] = gp.prev_p.clone();
+
+                let dist = gp.p.dist(dst);
+                if dist < nearest_dist {
+                    nearest_dist = dist;
+                    nearest_point = gp.p.clone();
+                }
+
+                if gp.p.x > 0 {
+                    raise_points.push_back(
+                        GrowPoint {
+                            p : TVec2 {x : gp.p.x - 1, y : gp.p.y},
+                            prev_p : gp.p.clone(),
+                            val : gp.val + 1
+                        }
+                    )
+                }
+
+                if gp.p.y > 0 {
+                    raise_points.push_back(
+                        GrowPoint {
+                            p : TVec2 {x : gp.p.x, y : gp.p.y - 1},
+                            prev_p : gp.p.clone(),
+                            val : gp.val + 1
+                        }
+                    )
+                }
+
+                if gp.p.x < d_w {
+                    raise_points.push_back(
+                        GrowPoint {
+                            p : TVec2 {x : gp.p.x + 1, y : gp.p.y},
+                            prev_p : gp.p.clone(),
+                            val : gp.val + 1
+                        }
+                    )
+                }
+
+                if gp.p.y < d_h {
+                    raise_points.push_back(
+                        GrowPoint {
+                            p : TVec2 {x : gp.p.x, y : gp.p.y + 1},
+                            prev_p : gp.p.clone(),
+                            val : gp.val + 1
+                        }
+                    )
+                }
+            }
+        }
+
+        //move back
+        
+        let mut tmp_point = nearest_point;
+        let mut cur_val = self.data[tmp_point.y * self.w + tmp_point.x];
+
+        while cur_val > 2 {
+            tmp_point = self.prev_point[tmp_point.y * self.w + tmp_point.x].clone();
+            cur_val = self.data[tmp_point.y * self.w + tmp_point.x];
+        }
+        
+        tmp_point
+    }
+
+    pub fn new(w : usize, h : usize) -> Self {
+        Self {
+            w,
+            h,
+            data : vec![0; w * h],
+            walls : vec![false; w * h],
+            prev_point : vec![TVec2::default(); w * h]
+        }
+    }
+}
+
 pub struct Map {
     pub data : Vec<Tile>,
     pub w : u32,
     pub h : u32,
     pub my_scrap : i32,
-    pub enemy_scrap : i32
+    pub enemy_scrap : i32,
+    pub pathfinder : Pathfinder
 }
 
 
@@ -95,7 +238,51 @@ impl Map {
             }
         }
 
+        //setup walls
+        for idx in 0..self.data.len() {
+            let tile = &self.data[idx];
+            let wall = tile.recycler || tile.scrap_amount == 0;
+            self.pathfinder.walls[idx] = wall;
+        }
+
         //move
+        for a in my_actions.iter() {
+            if let Action::Move(mv) = a {
+                let idx = (mv.fromX * self.w + mv.fromY) as usize;
+               
+                if self.data[idx].owner == TileOwner::Me && self.data[idx].units > 0 {
+                    let move_amount = self.data[idx].units.min(mv.amount as i32);
+                    let dst = self.pathfinder.find_path(
+                        &TVec2::new(mv.fromX as usize, mv.fromY as usize), 
+                        &TVec2::new(mv.toX as usize, mv.toY as usize));
+                    self.data[dst.y * self.w as usize + dst.x].delta_units += move_amount;
+                    self.data[idx].units -= move_amount;
+                }
+            } else if let Action::Spawn(sp) = a {
+                let cost = sp.amount as i32 * 10;
+                if self.my_scrap >= cost {
+                    let tile = &mut self.data[(sp.x * self.w + sp.y) as usize];
+                    tile.delta_units += sp.amount as i32;
+                    self.my_scrap -= cost;
+                }
+                
+            }
+        }
+
+        for a in enemy_actions.iter() {
+            if let Action::Move(mv) = a {
+                let idx = (mv.fromX * self.w + mv.fromY) as usize;
+               
+                if self.data[idx].owner == TileOwner::Enemy && self.data[idx].units < 0 {
+                    let move_amount = (-self.data[idx].units).min(mv.amount as i32);
+                    let dst = self.pathfinder.find_path(
+                        &TVec2::new(mv.fromX as usize, mv.fromY as usize), 
+                        &TVec2::new(mv.toX as usize, mv.toY as usize));
+                    self.data[dst.y * self.w as usize + dst.x].delta_units -= move_amount;
+                    self.data[idx].units += move_amount;
+                }
+            }
+        }
 
         //perturn scrap increase
         self.my_scrap += 10;
@@ -142,7 +329,8 @@ impl Map {
                     owner : tile_owner,
                     units,
                     scrap_amount,
-                    recycler : (recycler == 1)
+                    recycler : (recycler == 1),
+                    delta_units : 0
                 };
 
                 data[(y * width + x) as usize] = tile;
@@ -155,7 +343,8 @@ impl Map {
             w : width,
             h : height,
             my_scrap,
-            enemy_scrap
+            enemy_scrap,
+            pathfinder : Pathfinder::new(width as usize, height as usize)
         }
     }
 
@@ -174,5 +363,13 @@ mod tests {
     #[test]
     fn test_load_map() {
         Map::load(String::from_str("14 7;10 10;9 -1 0 0 0 0 0;6 -1 0 0 0 0 0;8 -1 0 0 0 0 0;8 -1 0 0 0 0 0;4 -1 0 0 0 0 0;4 -1 0 0 0 0 0;8 -1 0 0 0 0 0;10 -1 0 0 0 0 0;6 -1 0 0 0 0 0;6 -1 0 0 0 0 0;10 -1 0 0 0 0 0;4 -1 0 0 0 0 0;8 -1 0 0 0 0 0;6 -1 0 0 0 0 0;8 -1 0 0 0 0 0;0 -1 0 0 0 0 0;6 -1 0 0 0 0 0;9 -1 0 0 0 0 0;6 -1 0 0 0 0 0;8 -1 0 0 0 0 0;8 -1 0 0 0 0 0;8 -1 0 0 0 0 0;4 -1 0 0 0 0 0;9 0 1 0 0 0 0;4 -1 0 0 0 0 0;6 -1 0 0 0 0 0;8 -1 0 0 0 0 0;9 -1 0 0 0 0 0;0 -1 0 0 0 0 0;0 -1 0 0 0 0 0;9 -1 0 0 0 0 0;8 -1 0 0 0 0 0;6 -1 0 0 0 0 0;9 -1 0 0 0 0 0;9 -1 0 0 0 0 0;9 -1 0 0 0 0 0;8 0 1 0 0 0 0;10 0 0 0 0 0 0;10 0 1 0 0 0 0;9 -1 0 0 0 0 0;4 -1 0 0 0 0 0;8 -1 0 0 0 0 0;8 -1 0 0 0 0 0;6 -1 0 0 0 0 0;4 -1 0 0 0 0 0;8 -1 0 0 0 0 0;8 1 1 0 0 1 0;8 -1 0 0 0 0 0;8 -1 0 0 0 0 0;8 -1 0 0 0 0 0;8 -1 0 0 0 0 0;8 0 1 0 0 0 0;8 -1 0 0 0 0 0;4 -1 0 0 0 0 0;6 -1 0 0 0 0 0;8 -1 0 0 0 0 0;8 -1 0 0 0 0 0;4 -1 0 0 0 0 0;9 -1 0 0 0 0 0;10 1 1 0 0 1 0;10 1 0 0 1 1 0;8 1 1 0 0 1 0;9 -1 0 0 0 0 0;9 -1 0 0 0 0 0;9 -1 0 0 0 0 0;6 -1 0 0 0 0 0;8 -1 0 0 0 0 0;9 -1 0 0 0 0 0;0 -1 0 0 0 0 0;0 -1 0 0 0 0 0;9 -1 0 0 0 0 0;8 -1 0 0 0 0 0;6 -1 0 0 0 0 0;4 -1 0 0 0 0 0;9 1 1 0 0 1 0;4 -1 0 0 0 0 0;8 -1 0 0 0 0 0;8 -1 0 0 0 0 0;8 -1 0 0 0 0 0;6 -1 0 0 0 0 0;9 -1 0 0 0 0 0;6 -1 0 0 0 0 0;0 -1 0 0 0 0 0;8 -1 0 0 0 0 0;6 -1 0 0 0 0 0;8 -1 0 0 0 0 0;4 -1 0 0 0 0 0;10 -1 0 0 0 0 0;6 -1 0 0 0 0 0;6 -1 0 0 0 0 0;10 -1 0 0 0 0 0;8 -1 0 0 0 0 0;4 -1 0 0 0 0 0;4 -1 0 0 0 0 0;8 -1 0 0 0 0 0;8 -1 0 0 0 0 0;6 -1 0 0 0 0 0;9 -1 0 0 0 0 0").unwrap());
+    }
+
+    #[test]
+    fn test_dummy_pathfinder() {
+        let mut pathfinder = Pathfinder::new(32, 32);
+        let res = pathfinder.find_path(&TVec2::new(0, 0) , &TVec2::new(0, 10));
+
+        assert_eq!(res, TVec2::new(0, 1));
     }
 }
